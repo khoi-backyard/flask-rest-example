e6c84e1d00991d797688709dc8118dbb880f3eb4
khoi <khoiracle@gmail.com>
2020-03-06 15:35:34 +0700

    Tweak SQLALCHEMY development config

--- config.py
+++ config.py
@@ -8,13 +8,12 @@ class BaseConfig(object):
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}" \
         .format(DB_USER="khoi", DB_PASS="",
                 DB_ADDR="127.0.0.1",
                 DB_NAME="bluebox")
-    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 
 class ProductionConfig(BaseConfig):
